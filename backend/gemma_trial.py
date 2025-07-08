a243943bfd34e905317e0df8f03adfd521e40016
KarthikPrevin <karthik.previn@gmail.com>
2025-07-08 23:51:08 +0530

    could work better

--- backend/gemma_trial.py
+++ backend/gemma_trial.py
@@ -1,8 +1,9 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 import os 
+import faiss
 
 load_dotenv()
 key = os.getenv("GOOGLE_API_KEY")
@@ -11,30 +12,55 @@ if key==None:
     exit()
 os.environ["GOOGLE_API_KEY"] = key
 
-
-
-emb_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
 texts = [
+    "You are gemma3n",
+    "i call you jarvis",
     "i am Rise of Devil",
     "i like chocolate milkshake",
     "u serve me for eternity"
 ]
 
-
-db = FAISS.from_texts(texts,emb_model)
-query = "Who am i?"
-results = db.similarity_search(query, k=2)
-
-for i, doc in enumerate(results):
-    print(f"Result {i+1}: {doc.page_content}")
-
-
+memory_prompt = """
+REPLY USING CONTEXT MEMORY BELOW:
+{memory}
+PROMPT:
+{prompt}
+"""
 
 llm = ChatGoogleGenerativeAI(model="gemma-3n-e4b-it", temperature=0.7)
+emb_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.from_texts(["memory"],emb_model)
+# d = emb_model.client.get_sentence_embedding_dimension()
+# index = faiss.IndexFlatL2(d)
+
+def add_text(text):
+    db.add_texts([text])
+
+def search(question,k=2):
+    return [i.page_content for i in db.similarity_search(question,k=k)]
+
+def ask(question):
+    res = search(question)
+    prepared_prompt = memory_prompt.format(memory = "\n".join(res),prompt=question)
+    response = llm.invoke(prepared_prompt)
+    db.add_texts([question])
+    return response
+
+
+
+for i in texts:
+    add_text(i)
+
+a1 = ask("who are you?")
+a2 = ask("who am i?")
+
+print(a1)
+print(a2)
+
+
 
 # Basic prompt
-response = llm.invoke("who are you?")
-print(response)
+# response = llm.invoke("who are you?")
+# print(response)
 
 ##add vector embedings for memory
